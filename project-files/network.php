f6d0d8adc0f701858c48fa3c4a3a03eb3e38902e
SimoTaf <stafifet@inttic.dz>
2023-02-15 17:09:51 +0100

    added my project first version

---
+++ project-files/network.php
@@ -0,0 +1,11 @@
+<?php 
+echo "Hi everyone , this is my little project using nmap-apache-php in order to scan my network using cron.";
+
+echo date("h:i:sa");
+
+echo "<pre>"; 
+
+include("nmap.html");
+
+echo "</pre>";
+?>
